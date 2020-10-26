a6df26ad845060190848edf6478368e4f33d211a
Luis Reyes <luisjavier_iza@hotmail.com>
2020-10-26 13:05:40 -0600

    corrigiendo error

--- red-bicicletas/app.js
+++ red-bicicletas/app.js
@@ -24,7 +24,6 @@ mongoose.Promise = global.Promise;
 var dbConnection = mongoose.connection;
 dbConnection.on('error',console.error.bind(console,'MongoDB Connection Error'));
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -40,18 +39,14 @@ app.use('/bicicleta',biciRouter);
 app.use('/apiBicicletas',apiBici);
 app.use('/apiUsuarios', usuariosAPIRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
