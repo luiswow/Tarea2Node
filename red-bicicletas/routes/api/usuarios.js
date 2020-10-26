d1491fe0579e1cda4104cd31d8437fa28ab91dad
Luis Reyes <luisjavier_iza@hotmail.com>
2020-10-26 13:04:45 -0600

    Arreglo de error

--- red-bicicletas/routes/api/usuarios.js
+++ red-bicicletas/routes/api/usuarios.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var usuarioController= require('../../api/usuarioControllerAPI')
 
-/* GET users listing. */
 router.get('/', usuarioController.usuario_list);
 router.post('/create', usuarioController.usuario_create);
 router.post('/reservar', usuarioController.usuario_reservar);
--- red-bicicletas/routes/bicicletaRoutes.js
+++ red-bicicletas/routes/bicicletaRoutes.js
@@ -1,11 +1,7 @@
-//nos traemos el modulo de routes de express
 var express = require('express');
-//modulo routeador de express
 var router = express.Router();
-//importamos el controlador
 var biciCntroller = require('../controller/bicicletaController');
 
-//definimos el metodo http 
 router.get('/',biciCntroller.bicicleta_list);
 router.get('/create',biciCntroller.bicicleta_create_get);
 router.post('/create',biciCntroller.bicicleta_create_post);
